3b5a7db131f73da102ced7a2660306d039a64ea1
rimasultana <rimasultana20225@gmail.com>
2024-03-13 22:51:20 +0600

    object

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// const person = "Jhon dhao";
+// // const person = "Jhon dhao";
 
-// console.log(person);
+// // console.log(person);
 
-let person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  eyeColor: "blue",
-};
- console.log(person.firstName + " " + person.lastName);
+// let person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue",
+// };
+//  console.log(person.firstName + " " + person.lastName);
+
+
+ const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue"; 
+
+console.log(person.firstName + " is " + person.age + " years old ");
