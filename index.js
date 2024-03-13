9719cdc41e5f92a8194d0b38d1b6a5f61806b812
rimasultana <rimasultana20225@gmail.com>
2024-03-13 22:45:07 +0600

    object definetion

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const person = "Jhon dhao";
+// const person = "Jhon dhao";
 
-console.log(person);
+// console.log(person);
+
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+ console.log(person.firstName + " " + person.lastName);
