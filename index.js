65d77ddeaab7b940aa17d182f606813b70fd12f5
rimasultana <rimasultana20225@gmail.com>
2024-03-13 22:53:23 +0600

    object

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ person.age = 50;
 person.eyeColor = "blue"; 
 
 console.log(person.firstName + " is " + person.age + " years old ");
+
+
+
+const person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50, eyeColor:"blue"
+  }
+  
+  const person2 = person;
+  person2.age = 10; 
+
+
+  console.log(person2);
